05ab3b7d22924f2b4bd1e2f815544a717b3dfc56
Sushma Imaginea <sushma.maddirala@imaginea.com>
2017-08-29 10:37:54 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "flyer",
+  "defaultLanguage" : "en",
+  "displayName" : "SalesVision_29Aug",
+  "homePage" : "Main",
+  "name" : "SalesVision_29Aug",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
